a7d76c75105fec934ceca067e2ea71b6bf657e57
Knight-Thomas <thomasmilesknight@icloud.com>
2025-03-21 12:51:36 +0000

    Changing layout of buttons at the bottom of the pages

---
+++ BS-App/BS-App/BS_AppMainPage.swift
@@ -0,0 +1,8 @@
+//
+//  BS_AppMainPage.swift
+//  BS-App
+//
+//  Created by Tom Knight on 20/03/2025.
+//
+
+import Foundation
---
+++ BS-App/BS-App/BS_AppMapPage.swift
@@ -0,0 +1,8 @@
+//
+//  BS_AppMapPage.swift
+//  BS-App
+//
+//  Created by Tom Knight on 21/03/2025.
+//
+
+import Foundation
